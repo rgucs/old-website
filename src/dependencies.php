d0810e8a64459dbe60155fe5852d25e2c7eb2b24
Luke Fisher <mail@lukefisher.me>
2016-10-03 22:49:51 +0100

    Slim Skeleton commit

---
+++ src/dependencies.php
@@ -0,0 +1,36 @@
+<?php
+// DIC configuration
+
+$container = $app->getContainer();
+
+// view renderer
+$container['renderer'] = function ($c) {
+    $settings = $c->get('settings')['renderer'];
+    return new Slim\Views\PhpRenderer($settings['template_path']);
+};
+
+// Register component on container
+$container['view'] = function ($c) {
+    $settings = $c->get('settings')['view'];
+
+    $view = new \Slim\Views\Twig($settings['template_path'], [
+        'cache' => $settings['cache_path'],
+        'auto_reload' => true
+    ]);
+
+    $view->addExtension(new \Slim\Views\TwigExtension(
+        $c['router'],
+        $c['request']->getUri()
+    ));
+
+    return $view;
+};
+
+// monolog
+$container['logger'] = function ($c) {
+    $settings = $c->get('settings')['logger'];
+    $logger = new Monolog\Logger($settings['name']);
+    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+    return $logger;
+};
---
+++ src/settings.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // View settings
+        'view' => [
+            'template_path' => __DIR__ . '/../templates/',
+            'cache_path' => __DIR__ . '/../cache/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+    ],
+];
