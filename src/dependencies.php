97e3aa021c7cb03a0449b08b4198f2388af5436a
Luke Fisher <mail@lukefisher.me>
2016-10-13 09:51:24 +0100

    Update the dependencies and router config

--- src/dependencies.php
+++ src/dependencies.php
@@ -4,33 +4,33 @@
 $container = $app->getContainer();
 
 // view renderer
-$container['renderer'] = function ($c) {
-    $settings = $c->get('settings')['renderer'];
-    return new Slim\Views\PhpRenderer($settings['template_path']);
-};
-
-// Register component on container
 $container['view'] = function ($c) {
-    $settings = $c->get('settings')['view'];
+  $settings = $c->get('settings')['view'];
 
-    $view = new \Slim\Views\Twig($settings['template_path'], [
-        'cache' => $settings['cache_path'],
-        'auto_reload' => true
-    ]);
+  $view = new \Slim\Views\Twig($settings['template_path'], [
+    'cache' => $settings['cache_path'],
+    'debug' => true
+  ]);
 
-    $view->addExtension(new \Slim\Views\TwigExtension(
-        $c['router'],
-        $c['request']->getUri()
-    ));
+  $view->addExtension(new \Slim\Views\TwigExtension(
+    $c['router'],
+    $c['request']->getUri()
+  ));
 
-    return $view;
+  return $view;
 };
 
 // monolog
 $container['logger'] = function ($c) {
-    $settings = $c->get('settings')['logger'];
-    $logger = new Monolog\Logger($settings['name']);
-    $logger->pushProcessor(new Monolog\Processor\UidProcessor());
-    $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
-    return $logger;
+  $settings = $c->get('settings')['logger'];
+  $logger = new Monolog\Logger($settings['name']);
+  $logger->pushProcessor(new Monolog\Processor\UidProcessor());
+  $logger->pushHandler(new Monolog\Handler\StreamHandler($settings['path'], $settings['level']));
+  return $logger;
+};
+
+$container['notFoundHandler'] = function($c) {
+  return function($request, $response) use ($c) {
+    return $c['view']->render($response, '404.twig');
+  };
 };
--- src/routes.php
+++ src/routes.php
@@ -1,10 +1,3 @@
 <?php
-// Routes
-
-$app->get('/[{name}]', function ($request, $response, $args) {
-    // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
-
-    // Render index view
-    return $this->view->render($response, 'home.twig', $args);
-});
+// Main Routes
+$app->get('/', '\Controller\MainController:index')->setName('index');
