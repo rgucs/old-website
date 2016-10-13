db64d26ec2184327feee0e2be143cb1293a571db
Luke Fisher <mail@lukefisher.me>
2016-10-13 23:22:01 +0100

    Add in new controllers + templates

---
+++ src/Controller/PollController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Site\Controller;
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+class PollController extends Controller {
+  public function index(Request $request, Response $response, $args) {
+    // Load all of the Polls here
+    $polls = $this->ci->database->table('polls')->get();
+
+    return $this->ci->view->render($response, 'polls.twig', [
+      'polls' => $polls
+    ]);
+  }
+
+  public function poll(Request $request, Response $response, $args) {
+    // Load a specific Poll here
+  }
+
+  public function vote(Request $request, Response $response, $args) {
+    // Submit a vote against a Poll
+  }
+}
