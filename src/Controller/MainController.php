689e8a9d900f5d78a748b9eb8eaab8dd2bc663ec
Luke Fisher <mail@lukefisher.me>
2016-10-31 14:53:11 +0000

    Update Controllers to explicitly return ResponseInterface

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -6,16 +6,16 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 class MainController extends Controller {
-  public function index(Request $request, Response $response, $args) {
+  public function index(Request $request, Response $response, $args) : Response {
     // Render index view
     return $this->ci->view->render($response, 'home.twig');
   }
 
-  public function about(Request $request, Response $response, $args) {
+  public function about(Request $request, Response $response, $args) : Response {
     return $this->ci->view->render($response, 'about.twig');
   }
 
-  public function join_us(Request $request, Response $response, $args) {
+  public function join_us(Request $request, Response $response, $args) : Response {
     return $this->ci->view->render($response, 'join_us.twig');
   }
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,11 +6,11 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 class UserController extends Controller {
-  public function index(Request $request, Response $response, $args) {
+  public function index(Request $request, Response $response, $args) : Response {
 
   }
 
-  public function event(Request $request, Response $response, $args) {
+  public function event(Request $request, Response $response, $args) : Response {
 
   }
 }
