f243f891f295d9a9fb0e4c420635a37890c61fb3
Luke Fisher <mail@lukefisher.me>
2016-10-31 15:35:08 +0000

    Add in event page to show individual event details

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,6 +2,7 @@
 
 namespace Site\Controller;
 
+use \Slim\Exception\NotFoundException;
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
@@ -18,6 +19,17 @@ class EventController extends Controller {
   }
 
   public function event(Request $request, Response $response, $args) : Response {
+    // Load the event from the database
+    $event = $this->ci->database->table('events')
+      ->where('id', $args['id'])
+      ->first();
 
+    if ($event != null) {
+      return $this->ci->view->render($response, 'event.twig', [
+        'event' => $event
+      ]);
+    }
+
+    throw new NotFoundException($request, $response);
   }
 }
