d3e738258e6d7562983748116fe647cf5a16a670
Luke Fisher <mail@lukefisher.me>
2017-03-24 10:13:09 +0000

    Add in column for poll

--- sql/poll_choice.sql
+++ sql/poll_choice.sql
@@ -2,6 +2,7 @@ CREATE TABLE `poll_choice` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
   `poll_id` int(11) unsigned NOT NULL,
   `choice` text NOT NULL,
+  `image` char(24) DEFAULT '',
   PRIMARY KEY (`id`),
   KEY `fk_poll_choice` (`poll_id`),
   CONSTRAINT `fk_poll_choice` FOREIGN KEY (`poll_id`) REFERENCES `poll` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
